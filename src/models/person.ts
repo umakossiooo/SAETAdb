9b3fdfa2bea094e5212ad3cd2e5f9e33c3f09482
umakossiooo <a01254619@tec.mx>
2024-07-26 18:14:24 -0700

    feat: added the person model

---
+++ src/models/person.ts
@@ -0,0 +1,65 @@
+// person.ts
+import { Table, Column, Model, DataType, CreatedAt, UpdatedAt, DeletedAt, HasOne } from 'sequelize-typescript';
+import { Optional } from 'sequelize';
+import { Client } from './client';
+
+
+interface PersonAttributes {
+    id: string;
+    name: string;
+    lastName: string;
+    lastName2: number;
+    age: number;
+    job: string;
+    phoneNumber: string;
+    startingDay: Date;
+    email: string;
+    clientInformation: Client;
+    activeDB: boolean;
+}
+
+export interface PersonCreationAttributes extends Optional<PersonAttributes, 'id' | 'activeDB' | "clientInformation"> { }
+
+@Table({
+    tableName: 'person',
+    timestamps: true,
+    paranoid: true,
+})
+export class Person extends Model<PersonAttributes, PersonCreationAttributes> {
+
+    @Column(DataType.STRING(128))
+    public name!: string;
+
+    @Column(DataType.STRING(128))
+    public lastName!: string;
+
+    @Column(DataType.STRING(128))
+    public lastName2!: string;
+
+    @Column(DataType.DOUBLE(2))
+    public age!: number;
+
+    @Column(DataType.STRING(128))
+    public gender!: string;
+
+
+
+    @CreatedAt
+    @Column
+    public createdAt!: Date;
+
+    @UpdatedAt
+    @Column
+    public updatedAt!: Date;
+
+    @DeletedAt
+    @Column
+    public deletedAt!: Date;
+
+    // Default true
+    @Column({ type: DataType.BOOLEAN, defaultValue: true })
+    public activeDB!: boolean;
+
+    @HasOne(() => Client)
+    public clientInformation!: Client
+}
