6589aea7e75a61ae0c9a1d405ab16bd121de5032
umakossiooo <a01254619@tec.mx>
2024-07-26 18:04:24 -0700

    fix: updated client model

--- src/models/client.ts
+++ src/models/client.ts
@@ -1,15 +1,20 @@
 import {Table, Model, Column, CreatedAt, UpdatedAt, DataType} from 'sequelize-typescript';
 import {Optional} from 'sequelize';
+import { Person } from './person';
 
 interface ClientAttributes{
   id: string;
   personId:string;
+  personInformation: Person;
+  activeDB: boolean;
 }
 
-interface ClientCreationAttributes extends Optional<ClientAttributes, 'id'>{}
+interface ClientCreationAttributes extends Optional<ClientAttributes, 'id' | 'activeDB' | 'personInformation'>{}
 
 @Table ({
-  tableName: "Clients"
+  tableName: 'client',
+  timestamps: true,
+  paranoid: true,
 })
 export class Client extends Model<ClientAttributes, ClientCreationAttributes>{
 
